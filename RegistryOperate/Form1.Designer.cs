1d065ba784082165db3393ee8f02e029083e1412
admin <admin@DESKTOP-KED9K5G>
2017-12-24 13:15:01 +0800

    添加项目文件。

---
+++ RegistryOperate/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RegistryOperate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxRegName = new System.Windows.Forms.TextBox();
+            this.tbxRegDesc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxRegPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.tbxDllPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入注册项的名称";
+            // 
+            // tbxRegName
+            // 
+            this.tbxRegName.Location = new System.Drawing.Point(31, 70);
+            this.tbxRegName.Name = "tbxRegName";
+            this.tbxRegName.Size = new System.Drawing.Size(135, 21);
+            this.tbxRegName.TabIndex = 1;
+            // 
+            // tbxRegDesc
+            // 
+            this.tbxRegDesc.Location = new System.Drawing.Point(241, 70);
+            this.tbxRegDesc.Multiline = true;
+            this.tbxRegDesc.Name = "tbxRegDesc";
+            this.tbxRegDesc.Size = new System.Drawing.Size(135, 21);
+            this.tbxRegDesc.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "请输入注册项的描述";
+            // 
+            // tbxRegPath
+            // 
+            this.tbxRegPath.Location = new System.Drawing.Point(443, 70);
+            this.tbxRegPath.Name = "tbxRegPath";
+            this.tbxRegPath.Size = new System.Drawing.Size(135, 21);
+            this.tbxRegPath.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(423, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "请输入增加或删除的注册项的路径";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(713, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "注册";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(713, 180);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 8;
+            this.btnDel.Text = "删除注册";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(31, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(631, 340);
+            this.listBox1.TabIndex = 9;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(713, 434);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 10;
+            this.btnCopy.Text = "注册表备份";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // tbxDllPath
+            // 
+            this.tbxDllPath.Location = new System.Drawing.Point(653, 70);
+            this.tbxDllPath.Name = "tbxDllPath";
+            this.tbxDllPath.Size = new System.Drawing.Size(135, 21);
+            this.tbxDllPath.TabIndex = 12;
+            this.tbxDllPath.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbxDllPath_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(675, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "请输入插件路径";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 507);
+            this.Controls.Add(this.tbxDllPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbxRegPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxRegDesc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxRegName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoCAD插件注册表注册";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxRegName;
+        private System.Windows.Forms.TextBox tbxRegDesc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxRegPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox tbxDllPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RegistryOperate/Form1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace RegistryOperate
+{
+    public partial class Form1 : Form
+    {
+        private static string name = null;
+        private static string desc = null;
+        private static string path = null;
+        private static string dllPath = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            tbxRegDesc.Text = "ppp";
+            tbxRegName.Text = "psw_plugin";
+            tbxRegPath.Text = @"SOFTWARE\Autodesk\AutoCAD\R18.2\ACAD-A001:804\Applications\";
+        }
+
+
+
+        private void ReadMsg()
+        {
+            name = tbxRegName.Text.Trim();
+            desc = tbxRegDesc.Text.Trim();
+            path = tbxRegPath.Text.Trim();
+            dllPath = tbxDllPath.Text.Trim();
+        }
+        /// <summary>
+        /// 增加注册表，根据输入的名称、描述和路径
+        /// </summary>
+        /// <param name="name">注册表项目名称</param>
+        /// <param name="desc">项目描述</param>
+        /// <param name="path">注册表项目的路径</param>
+        private static void AddRegister(string name, string desc, string path, string dllPath)
+        {
+            RegistryKey localMachine = Registry.LocalMachine;
+            RegistryKey app = localMachine.OpenSubKey(path, true);
+            RegistryKey myKey = app.CreateSubKey(name);
+            myKey.SetValue("DESCRIPTION", desc, RegistryValueKind.String);
+            myKey.SetValue("LOADCTRLS", 2, RegistryValueKind.DWord);
+            myKey.SetValue("LOADER", dllPath, RegistryValueKind.String);
+            myKey.SetValue("MANAGED", 1, RegistryValueKind.DWord);
+            localMachine.Close();
+            app.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            ReadMsg();
+            DialogResult res = MessageBox.Show("请确认注册表路径和表项名称!" + "\n" + path + name, "提醒", MessageBoxButtons.YesNo);
+
+            if (res == DialogResult.Yes)
+            {
+                try
+                {
+
+                    if (!"".Equals(name) && !"".Equals(desc) && !"".Equals(path) && !"".Equals(dllPath))
+                    {
+                        AddRegister(name, desc, path, dllPath);
+                        listBox1.Items.Add("注册" + name + "成功");
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("注册失败，有路径未填写！");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add("注册" + name + "失败，请检查路径输入项" + "\t\n" + ex.Message);
+                }
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            ReadMsg();
+
+            DialogResult res = MessageBox.Show("请确认注册表路径和表项名称!" + "\n" + path + name, "提醒", MessageBoxButtons.YesNo);
+
+            if (res == DialogResult.Yes)
+            {
+                try
+                {
+                    RegistryKey key = Registry.LocalMachine;
+                    key.DeleteSubKeyTree(path + "\\" + name, true);
+                    key.Close();
+                    listBox1.Items.Add("删除成功");
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add("删除失败，遇到错误，请检查路径" + "\t\n" + ex.Message);
+                }
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //判断项目是否存在
+        private bool IsRegeditItemExist(RegistryKey RegBoot, string ItemName)
+        {
+            if (ItemName.IndexOf("\\") <= -1)
+            {
+                string[] subkeyNames;
+                subkeyNames = RegBoot.GetValueNames();
+                foreach (string ikeyName in subkeyNames)  //遍历整个数组
+                {
+                    if (ikeyName == ItemName) //判断子项的名称
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                string[] strkeyNames = ItemName.Split('\\');
+                RegistryKey _newsubRegKey = RegBoot.OpenSubKey(strkeyNames[0]);
+                string _newRegKeyName = "";
+                int i;
+                for (i = 1; i < strkeyNames.Length; i++)
+                {
+                    _newRegKeyName = _newRegKeyName + strkeyNames[i];
+                    if (i != strkeyNames.Length - 1)
+                    {
+                        _newRegKeyName = _newRegKeyName + "\\";
+                    }
+                }
+                return IsRegeditItemExist(_newsubRegKey, _newRegKeyName);
+            }
+        }
+
+        //判断键值是否存在
+        private bool IsRegeditKeyExist(RegistryKey RegBoot, string RegKeyName)
+        {
+
+            string[] subkeyNames;
+            subkeyNames = RegBoot.GetValueNames();
+            foreach (string keyName in subkeyNames)
+            {
+
+                if (keyName == RegKeyName)  //判断键值的名称
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+        private void tbxDllPath_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                tbxDllPath.Text = path;
+            }
+        }
+
+
+    }
+}
